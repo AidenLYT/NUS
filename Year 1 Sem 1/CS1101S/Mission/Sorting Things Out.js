56973a13de258296cd94b118ea89b1336d78bce1
Aiden Lee <110450637+AidenLYT@users.noreply.github.com>
2022-10-17 02:11:16 +0800

    Update Sorting Things Out.js

--- Year 1 Sem 1/CS1101S/Mission/Sorting Things Out.js
+++ Year 1 Sem 1/CS1101S/Mission/Sorting Things Out.js
@@ -10,8 +10,8 @@ function partition(xs, p) {
 const my_list = list(1, 2, 3, 4, 5, 6);
 partition(my_list, 4);
 
-// Task 2
 
+// Task 2
 function partition(xs, p) {
     // your answer from Task 1
     return pair(filter(x => x <= p, xs), filter(x => x > p, xs));
@@ -29,6 +29,7 @@ function quicksort(xs) {
 // Test
 const my_list = list(23, 12, 56, 92, -2, 0);
 quicksort(my_list);
+
 
 //What is the order of growth in time for applying partition to a list of length n?
 //Θ(n)
